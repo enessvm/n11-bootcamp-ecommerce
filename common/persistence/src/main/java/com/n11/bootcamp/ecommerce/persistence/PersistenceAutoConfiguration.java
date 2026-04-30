719bff01046438dc6510f7a097f7891bf512ef55
Enes Sevim <sevim.enes06@gmail.com>
2026-04-30 19:03:17 +0300

    feat(common-persistence): add autoconfigure for Auditable class

---
+++ common/persistence/src/main/java/com/n11/bootcamp/ecommerce/persistence/PersistenceAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.n11.bootcamp.ecommerce.persistence;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@AutoConfiguration
+@EnableJpaAuditing
+public class PersistenceAutoConfiguration {
+}
