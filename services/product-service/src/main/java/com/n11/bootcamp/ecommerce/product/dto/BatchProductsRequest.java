52b24aba2e197a4c51a63294f83c5d862a01d51a
Enes Sevim <sevim.enes06@gmail.com>
2026-04-30 19:25:13 +0300

    chore(): unused import removed

--- services/product-service/src/main/java/com/n11/bootcamp/ecommerce/product/dto/BatchProductsRequest.java
+++ services/product-service/src/main/java/com/n11/bootcamp/ecommerce/product/dto/BatchProductsRequest.java
@@ -1,8 +1,6 @@
 package com.n11.bootcamp.ecommerce.product.dto;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
